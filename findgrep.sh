3fe1bc4b85d8a4876a0b78ffe7b61ad418051e19
Kaito Nakamatsu <n20019@std.it-college.ac.jp>
2020-06-24 11:54:01 +0900

    file pattern

--- findgrep.sh
+++ findgrep.sh
@@ -6,6 +6,9 @@ directory=$2
 if [ -z "$directory" ]; then
 	directory=','
 fi
+if [ -z "$name" ]; then
+	name='*'
+fi
 # -n : print line number
 # =H : print the file name
-find "$directory" -type f | xargs grep -nH "$patterni"
+find "$directory" -type f -name "$name"  | xargs grep -nH "$patterni"
