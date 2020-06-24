bae67a944d974e82553a1ce962296b012c38958c
Kaito Nakamatsu <n20019@std.it-college.ac.jp>
2020-06-24 11:29:33 +0900

    add comment

---
+++ findgrep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+pattrern=$1
+directory=$2
+if [ -z "$directory" ]; then
+	directory=','
+fi
+# -n : print line number
+# =H : print the file name:
+find "$directory" -type f | xargs grep -nH "$patterni"
