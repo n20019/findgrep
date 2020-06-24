4685cf0b941783d41120ab514e417544a3f236b3
Kaito Nakamatsu <n20019@std.it-college.ac.jp>
2020-06-24 11:43:20 +0900

    add comment

--- findgrep.sh
+++ findgrep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
+# $1 => search pattern, $2 => search directory
 pattrern=$1
 directory=$2
 if [ -z "$directory" ]; then
 	directory=','
 fi
 # -n : print line number
-# =H : print the file name:
+# =H : print the file name
 find "$directory" -type f | xargs grep -nH "$patterni"
